b2d1127e3e1d5533a27770db75a2b5754ac7ed4b
joaofelipe <joaofelipe1294@gmail.com>
2020-06-15 09:10:56 -0300

    Add attribute confirmation_status to team_user.

--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -3,4 +3,16 @@ class TeamUser < ApplicationRecord
   belongs_to :user
   validates_presence_of :team, :user
   validates_uniqueness_of :user_id, :scope => :team_id
+  before_validation :set_confirmation_status
+  enum confirmation_status: {
+    open: 0,
+    confirmed: 1
+  }
+
+  private
+
+    def set_confirmation_status
+      self.confirmation_status = :open unless self.confirmation_status
+    end
+
 end
--- spec/models/team_user_spec.rb
+++ spec/models/team_user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe TeamUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'create team_user' do
+    before :each do
+      @team_user = FactoryGirl.build(:team_user)
+    end
+    context 'when missing required params' do
+      after :each do
+        expect(@team_user).to be_invalid
+      end
+      it 'is invalid when without user' do
+        @team_user.user = nil
+      end
+      it 'is invalid when without team' do
+        @team_user.team = nil
+      end
+    end
+    it 'is expected to have default confirmation status as :open' do
+      expect(@team_user).to be_valid
+      expect(@team_user.open?).to be_truthy
+    end
+  end
+
 end
